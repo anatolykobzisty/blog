2fdee909a20ac8319127285743c10c71cefca49f
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-27 11:34:49 +0600

    Updates

---
+++ precache-manifest.bbe47b745b9353ee3e0a71df8582e709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defefb87fc25d90c86ae04c39055ff0a",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "25254dadcf431ecadeea",
+    "url": "/blog/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "25254dadcf431ecadeea",
+    "url": "/blog/static/js/2.c83e749c.chunk.js"
+  },
+  {
+    "revision": "b5de610e0b38ed2f33116f52a797c558",
+    "url": "/blog/static/js/2.c83e749c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e96dedb5aba8af313b",
+    "url": "/blog/static/js/main.a8f04494.chunk.js"
+  },
+  {
+    "revision": "34c7b270b978cd13aad9",
+    "url": "/blog/static/js/runtime-main.fb2521da.js"
+  }
+]);
