f974c059e5d85a5435b12bb2223d5e66dd2d10b5
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-03-04 15:19:01 +0600

    deleted yup

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -7,7 +7,6 @@ import { connect } from 'react-redux';
 
 import { Icon, Input, Button } from 'antd';
 import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 
 import styled from 'styled-components/macro';
 
@@ -44,21 +43,6 @@ const FormItem = styled.div`
 const ErrorMessage = styled.div`
   color: red;
 `;
-
-const validationSchema = Yup.object().shape({
-  username: Yup.string()
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
-  password: Yup.string()
-    .matches(
-      /^(?=^.{8,40}$)[0-9a-zA-Z](?=.*[A-Z])(?=.*[0-9])/,
-      'The password has to contain: A-Z, a-z, 0-9'
-    )
-    .required('Required'),
-});
 
 class Authentication extends Component {
   handleFormSubmit = (values, isSignUp) => {
@@ -100,9 +84,8 @@ class Authentication extends Component {
               password: '8Nkl8dsp',
             }}
             onSubmit={values => this.handleFormSubmit(values, isSignUp)}
-            validationSchema={validationSchema}
           >
-            {({ handleSubmit, values, handleChange, handleBlur, touched, errors }) => {
+            {({ handleSubmit, values, handleChange, handleBlur }) => {
               return (
                 <Form onSubmit={handleSubmit}>
                   {isSignUp && (
@@ -119,9 +102,6 @@ class Authentication extends Component {
                       {/* {errorMsgUsername && (
                         <ErrorMessage>Username {errorMsgUsername} </ErrorMessage>
                       )} */}
-                      {touched.username && errors.username && (
-                        <ErrorMessage>{errors.username}</ErrorMessage>
-                      )}
                     </FormItem>
                   )}
                   <FormItem>
@@ -135,7 +115,6 @@ class Authentication extends Component {
                       prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     />
                     {/* {errorMsgEmail && <ErrorMessage>Email {errorMsgEmail} </ErrorMessage>} */}
-                    {touched.email && errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
                   </FormItem>
                   <FormItem>
                     <Input
@@ -150,9 +129,6 @@ class Authentication extends Component {
                     {/* {errorMsgLogin && (
                       <ErrorMessage>Email or password {errorMsgLogin} </ErrorMessage>
                     )} */}
-                    {touched.password && errors.password && (
-                      <ErrorMessage>{errors.password}</ErrorMessage>
-                    )}
                   </FormItem>
                   <Button type="primary" htmlType="submit" loading={isLoading} block>
                     {pageTitle}
